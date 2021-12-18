2e11f6b1150735af5bb67b51875b72a7045ca894
lucasnasci17 <89095244+lucasnasci17@users.noreply.github.com>
2021-12-18 18:31:33 +0100

    version fonctionnelle 1

--- Projet/EDW_Academy/static/games/gamesDescription.js
+++ Projet/EDW_Academy/static/games/gamesDescription.js
@@ -1,13 +1,21 @@
-function afficherTextLol(){
-    return 'asélkdfhésalkfjélsakdfjlaskdf'
+// function afficherTextLol(){
+//     return 'Ceci est une popup pour LOL'
+// }
+//
+// function allerChercherPopup(e){
+//     e.classList.toggle("show")
+// }
+//
+// function afficherPopup(e){
+//     const button = document.getElementById("moreButton");
+//     button.addEventListener('click', e)
+// }
+
+
+function test1(){
+   return document.getElementById("myPopup");
 }
 
-function allerChercherPopup(e){
-    return e.classList.toggle("show")
+function test2(a){
+    a.classList.toggle("show")
 }
-
-function afficherPopup(e){
-    var button = document.getElementById("moreButton")
-    button.addEventListener('click', e)
-}
-
