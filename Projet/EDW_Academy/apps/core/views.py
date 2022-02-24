55359ec5d4bd3f56e35d7500550d74203ab60d17
lucas-mdrdn <lucas.mdrdn@eduge.ch>
2022-02-24 11:19:01 +0100

    changes for the second ajax dropdown list

--- Projet/EDW_Academy/apps/core/views.py
+++ Projet/EDW_Academy/apps/core/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 
--- Projet/EDW_Academy/static/improve_camp/mainIC.js
+++ Projet/EDW_Academy/static/improve_camp/mainIC.js
@@ -14,23 +14,19 @@ const gameText = document.getElementById('game-text')
 
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
-
-// $.ajax({
-//     type: 'GET',
-//     url: '/games-json/',
 function loaddoc() {
     const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-    if (this.readyState===4 && this.status===200){
-       console.log(JSON.parse(xhttp.response))
-        const gamesData = JSON.parse(xhttp.response)
-        for (let i = 0; i < gamesData['data'].length; i++) {
-            console.log(gamesData['data'][i])
-            const option = document.createElement('div')
-            option.textContent = gamesData['data'][i].name
-            option.setAttribute('class', 'item')
-            option.setAttribute('data-value', gamesData['data'][i].name)
-            gamesDataBox.appendChild(option)
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(JSON.parse(xhttp.response))
+            const gamesData = JSON.parse(xhttp.response)
+            for (let i = 0; i < gamesData['data'].length; i++) {
+                console.log(gamesData['data'][i])
+                const option = document.createElement('div')
+                option.textContent = gamesData['data'][i].name
+                option.setAttribute('class', 'item')
+                option.setAttribute('data-value', gamesData['data'][i].name)
+                gamesDataBox.appendChild(option)
             }
         }
     }
@@ -39,42 +35,38 @@ function loaddoc() {
 }
 loaddoc()
 
-    // error: function(error){
-    //     console.log(error)
-    // }
-// })
-gameInput.addEventListener('change', e=>{
+gameInput.addEventListener('change', e => {
     console.log(e.target.value)
     const selectedGame = e.target.value
 
-    // alertBox.innerHTML=""
-    // campsDataBox.innerHTML = ""
-    // campText.textContent = "Choose a camp"
-    // campText.classList.add("default")
+    alertBox.innerHTML = ""
+    campsDataBox.innerHTML = ""
+    campText.textContent = "Choose a camp"
+    campText.classList.add("default")
 
-    $.ajax({
-        type: 'GET',
-        url: `camps-json/${selectedGame}/`,
-        success: function(response){
-            console.log(response.data)
-            const campsData = response.data
-            campsData.map(item=>{
-                const option = document.createElement('div')
-                option.textContent = item.start_date + " - " + item.end_date
-                option.setAttribute('class', 'item')
-                option.setAttribute('data-value', item.start_date + " - " + item.end_date)
-                campsDataBox.appendChild(option)
-            })
-            // campInput.addEventListener('change', e=>{
-            //     btnBox.classList.remove('not-visible')})
-        },
-        error: function(error){
-            console.log(error)
+    function loadcamps() {
+        const xhttp2 = new XMLHttpRequest();
+        xhttp2.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(JSON.parse(xhttp2.response))
+                const campsData = JSON.parse(xhttp2.response)
+                for (let i = 0; i < campsData['data'].length; i++) {
+                    console.log(campsData['data'][i])
+                    const option = document.createElement('div')
+                    option.textContent = campsData['data'][i].start_date+" "+campsData['data'][i].end_date
+                    option.setAttribute('class', 'item')
+                    option.setAttribute('data-value', campsData['data'][i].start_date+" "+campsData['data'][i].end_date)
+                    campsDataBox.appendChild(option)
+                }
+            }
         }
-    })
+        xhttp2.open('GET', /camps-json/ + selectedGame, true);
+        xhttp2.send();
+    }
+    loadcamps()
 })
 
-gameForm.addEventListener('submit', e=>{
+gameForm.addEventListener('submit', e => {
     e.preventDefault()
     console.log('submitted')
 })
